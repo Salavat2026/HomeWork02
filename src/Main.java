035d5775be3a1bea4d308e7b0a73b20bb6302b12
Salavat2026 <Salavat92@bk.ru>
2026-02-21 16:03:58 +0500

    Решил ДЗ №2 с 1 по 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет мир");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("Задача №1");
+        System.out.println("dog="+dog);
+        System.out.println("cat="+cat);
+        System.out.println("paper="+paper);
+        System.out.println("Задача №2");
+        System.out.println("Увеличим значение каждой переменной на 4");
+        dog=dog+4;
+        System.out.println("dog="+dog);
+        cat=cat+4;
+        System.out.println("cat="+cat);
+        paper=paper+4;
+        System.out.println("paper="+paper);
+        System.out.println("Задача №3");
+        System.out.println("Уменьшить значение каждой переменной");
+        System.out.println ("dog уменьшить на 3,5" );
+        dog=dog-3.5;
+        System.out.println ("dog="+dog);
+        System.out.println ("cat уменьшить на 1.6");
+        cat=cat-1.6;
+        System.out.println ("cat="+cat);
+        System.out.println ("paper уменьшить на 7639");
+        paper=paper-7639;
+        System.out.println ("paper="+paper);
+        System.out.println ("Зaдача №4");
+var friend=19;//Инициализируем переменную значением 19
+        System.out.println("friend="+friend);//Выводим значение переменной в консоль
+        System.out.println("Увеличиваем значение переменной на 2");
+        friend=friend+2;//Увеличиваем значение переменной на 2
+        System.out.println("friend="+friend);//Выводим значение переменной в консоль
+        System.out.println("Делим на 7 ");
+        friend=friend/7;//Делим на 7
+        System.out.println("friend="+friend);
+
     }
 }
